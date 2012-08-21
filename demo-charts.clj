016138daa8b7e63e14653ea3ad818ff88aba4467
Claude Falguiere <cfalguiere@gmail.com>
2012-08-21 12:29:28 +0200

    demo charts

--- demo-charts.clj
+++ demo-charts.clj
@@ -1,17 +1,18 @@
 (ns statisticator
-  (:use (incanter core stats))
+  (:use (incanter core stats charts))
   (:require [clojure.contrib.generic.functor :as functor]
-	    [clj-time.coerce :as coerce] ) )
+	    [clj-time.coerce :as coerce]
+	    [clj-time.core :as clj-time]) )
 
 (load "jmeter-loader-util")
 
-(time (def ds (read-dataset-jmeter-file  "data/jmeter-sample.jtl") ))
+(time (def ds (read-dataset-jmeter-file  "data/sample-1h-m1.jtl") ))
 
 (def ds (conj-cols ds
 		   (col-names ($map #(coerce/from-long %) :ts ds )[:hrts]) ))
 
 
-;;; time series
+;;; time series of resp time
 (view (time-series-plot :ts :t :data ds))
 
 (doto  (time-series-plot :ts :t :data ds :title "Readings"
@@ -21,25 +22,34 @@
     (set-stroke-color java.awt.Color/blue)
     view)
 
-(view (histogram :ts :data ds) )
+;;; histogram of resp time
+(view (histogram :t :data ds) )
 
-(def groups  ($group-by :lb ds))
-(def counts (functor/fmap nrow groups) )
-(def categories (map #(:lb %) (keys counts)))
-(view (bar-chart categories (vals counts) :data ds :vertical false))
 
-(view (box-plot (sel ds :cols :t) :group-by (sel ds :cols :lb)))
+;;; bar-chart count per shop
+(view (bar-chart :shop :t :vertical false
+		 :data  ($rollup count :t :shop ds)))
 
-  (def y (sel ds :cols :t))
-  (def x (sel ds :cols :ts))
-  (def lm1 (linear-model y x))
+;;; box-plot 
+(view (box-plot :t :group-by :shop :data ds))
+
+;;; complex plot
+(def px (coerce/to-long (clj-time/date-time 2012 02 28 10 25)))
+(def py 12000)
+(def ts (sel ds :cols :ts))
+
 (doto  (time-series-plot :ts :t :data ds :title "Readings"
 			 :x-label "time" :y-label "resp. time (ms)"
 			 :legend true :series-label "duration ms"
 			 )
-  (add-lines  x (:fitted lm1) :series-label "regression")
-  (add-lines  x (repeat 3000) :series-label "max")
-  (add-pointer 10  6000 :text "ici" :angle :north) ;;TODO
-
-  (set-stroke-color java.awt.Color/blue ) ;;TODO colors
+  (add-lines  ts (repeat 3000) :series-label "threshold")
+  (add-polygon [[px py] [(+ px 100) (+ py 100)] [(- px 100) (+ py 10)]])
+  (add-pointer px py  :text "mais pourquoi ?" :angle :east) ;;TODO
+  (set-stroke-color java.awt.Color/blue :series 0) 
+  (set-stroke :width 1 :series 0) 
+  (set-stroke-color java.awt.Color/red :series 1) 
+  (set-stroke :width 3 :series 1) 
+  (set-stroke-color java.awt.Color/yellow :series 2) 
+  (set-stroke :width 5 :series 2) 
     view)
+
