4b0f7d6ca1f1fe0f259c2e07c2e9ba30c459dce1
Claude Falguiere <cfalguiere@gmail.com>
2012-08-21 19:21:04 +0200

    save the data as csv

--- demo-save.clj
+++ demo-save.clj
@@ -4,5 +4,10 @@
 (load "jmeter-loader-util")
 
 (def ds (read-dataset-jmeter-file  "data/sample-1h-m1.jtl") )
+(def ds (conj-cols ds
+		   (col-names ($map #(coerce/from-long %) :ts ds )[:hrts]) ))
+
+(save ds "data/readings.csv")
+
 (save ($rollup count :t :lb ds) "data/stats.csv")
 (save (time-series-plot :ts :t :data ds) "data/ts.png") 
