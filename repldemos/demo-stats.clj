80a0582c62061b71a823840db44783c6142e921a
Claude Falguiere <cfalguiere@gmail.com>
2012-08-23 14:45:13 +0200

    demo stats dans le projet repldemo

---
+++ repldemos/demo-stats.clj
@@ -0,0 +1,41 @@
+(use '(incanter core stats))
+
+(def ds (read-dataset  "../data/readings.csv" :header true) )
+
+(def ds (conj-cols ds
+		   (col-names ($map #(Long/parseLong %) :ts ds )[:tsn]) ))
+
+;;; count entries
+(nrow ds)
+
+;;; compute mean
+(mean ($ :t ds))
+
+;;; compute many indicators
+[ (count ($ :t ds)) (mean ($ :t ds)) (sd ($ :t ds)) ]
+;; cleaner
+(with-data ($ :t ds)
+  [ (count $data) (mean $data) (sd $data) (quantile $data :probs[0.95]) ] ) 
+;; insert labels
+(with-data ($ :t ds)
+  (zipmap [ "count" "mean" "sd" "q95"]
+	  [ (count $data) (mean $data) (sd $data) (quantile $data :probs[0.95]) ] )) 
+
+;;; groups
+(distinct ($ :lb ds))
+(keys ($group-by :lb ds))
+
+;;; dataset for a group
+(view (get ($group-by :lb ds) {:lb "SU01-HomePage-Page"} ))
+
+;; number of readings per group
+($rollup count :t :lb ds) ; yield a dataset
+
+;; mean per group
+($rollup mean :t :lb ds) ; yield a dataset
+
+;;q95 per group
+($rollup #(quantile % :probs[0.95])  :t :lb ds)
+
+
+
