f9aaaa169a39d9d42388645411ea6c8b5a528ac7
Claude Falguiere <cfalguiere@gmail.com>
2012-08-23 12:58:30 +0200

    added repldemos and moved demo-view

---
+++ repldemos/demo-view.clj
@@ -0,0 +1,33 @@
+(use '(incanter core io))
+
+(def ds (read-dataset  "../data/readings.csv" :header true) )
+
+;;; how many lines ?
+(nrow ds)
+
+;;; view data
+(view ds)
+(head ds)
+
+;;; select columns
+(view ($ :lb ds))
+(view ($ [:lb :t :shop] ds ))
+
+(view ($order :t :desc ds))
+
+
+;;; filtering values 
+(view ($where {:s true} ds))
+
+(distinct ($ :shop ds))
+(view ($where {:shop "MAF"} ds))
+(view ($ :t ($where {:shop "MAF"} ds)))
+(with-data ds
+	(view ($ :t ($where {:shop "MAF"}))))
+
+;; queries with multiple criteria
+(view ($where {:status true
+	       :shop {:$in #{"MAF", "FAB"}}
+	       :t {:$gt 3000 :$lt 5000}} ds))
+
+
---
+++ repldemos/project.clj
@@ -0,0 +1,10 @@
+(defproject repldemos "1.0.0-SNAPSHOT"
+  :description "demos incanter in repl"
+  :url "https://github.com/cfalguiere/ClojureBigDataJugSummerCamp2012p"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"
+            :distribution "repo"
+            :comments "Same license as Clojure"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [incanter "1.3.0"] ]
+  :dev-dependencies [[swank-clojure "1.4.2"]])
