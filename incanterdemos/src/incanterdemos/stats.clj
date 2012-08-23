fffe1467d2eb0953fddc83efe5b098a32913d0a7
Claude Falguiere <cfalguiere@gmail.com>
2012-08-23 20:25:33 +0200

    removed redundant incanter.core

--- incanterdemos/src/incanterdemos/stats.clj
+++ incanterdemos/src/incanterdemos/stats.clj
@@ -1,20 +1,19 @@
 (ns incanterdemos.stats
   (:use (incanter core))
   (:require [clojure.algo.generic.functor :as functor]
-	    [incanter.core :as incanter]
 	    [incanter.stats :as stats]
 	    [incanter.io :as incanterio]) )
 
 (defn dataset-stats-vector [ds]
-  (incanter/with-data (incanter/$ :t ds)
+  (with-data ($ :t ds)
     [ (count $data) (stats/mean $data) ] ))
 
 (defn group-stats-table [ds]
-   (incanter/dataset [:lb :count :mean]
+   (dataset [:lb :count :mean]
 	 (vals (functor/fmap
-		  #(flatten (conj [(incanter/sel % :rows 0 :cols :lb)]
+		  #(flatten (conj [(sel % :rows 0 :cols :lb)]
 				  (dataset-stats-vector % )))
-		  (incanter/$group-by :lb ds)))))
+		  ($group-by :lb ds)))))
 
 (defn -main [& args]
   (let [ds (incanterio/read-dataset  "resources/data/readings.csv" :header true)]
