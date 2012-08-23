3ca3e9373d36f15f707c989659f734c743b9a656
Claude Falguiere <cfalguiere@gmail.com>
2012-08-23 18:18:33 +0200

    new project incanter demo

---
+++ incanterdemos/src/incanterdemos/core.clj
@@ -0,0 +1,1 @@
+(ns incanterdemos.core)
---
+++ incanterdemos/test/incanterdemos/test/core.clj
@@ -0,0 +1,6 @@
+(ns incanterdemos.test.core
+  (:use [incanterdemos.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
